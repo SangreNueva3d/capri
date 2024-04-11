dc78be2d0376df5aca8a689b0128bd7e9b1d2450
SangreNueva3d <angel.yepes2@udea.edu.co>
2024-04-10 20:15:28 -0500

    Fice Commit

---
+++ src/main/java/culturemedia/service/impl/CultureMediaImpl.java
@@ -0,0 +1,27 @@
+package culturemedia.service.impl;
+
+import java.util.List;
+import culturemedia.model.Video;
+import culturemedia.model.View;
+import culturemedia.service.CultureMediaService;
+public class CultureMediaImpl implements CultureMediaService{
+
+    @Override
+    public Video add(Video video) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public View add(View view) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Video> findAllVideos() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
