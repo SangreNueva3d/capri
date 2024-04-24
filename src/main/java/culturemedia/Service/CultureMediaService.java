83782a291e3cb54c9dfffbca000dddf622cbadc4
SangreNueva3d <angel.yepes2@udea.edu.co>
2024-04-24 16:16:08 -0500

    9

--- src/main/java/culturemedia/Service/CultureMediaService.java
+++ src/main/java/culturemedia/Service/CultureMediaService.java
@@ -5,6 +5,8 @@ import culturemedia.exception.VideoNotFoundException;
 
 public interface CultureMediaService {
     List<Video> findAllVideos() throws VideoNotFoundException;
+    List<Video> find(String title) throws VideoNotFoundException;
+    List<Video> find(double fromDuration, double toDuration) throws VideoNotFoundException;
     Video add(Video video);
     View add(View view);
 }
