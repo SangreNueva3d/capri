038c233c0c73746f0a94fdbfd0a1fe05cc0cb153
SangreNueva3d <angel.yepes2@udea.edu.co>
2024-04-24 19:24:32 -0500

    10.1

--- src/main/java/culturemedia/controller/CultureMediaController.java
+++ src/main/java/culturemedia/controller/CultureMediaController.java
@@ -1,26 +1,22 @@
 package culturemedia.controller;
 
-import java.util.*;
+import java.util.List;
 
 import culturemedia.exception.VideoNotFoundException;
 import culturemedia.model.Video;
-import culturemedia.Service.impl.CultureMediaServiceImpl;
+import culturemedia.Service.CultureMediaService;
 
 public class CultureMediaController {
 
-	private final CultureMediaServiceImpl cultureMediaService;
+	private final CultureMediaService cultureMediaService;
 
-
-	public CultureMediaController(CultureMediaServiceImpl cultureMediaService) {
+	public CultureMediaController(CultureMediaService cultureMediaService) {
 		this.cultureMediaService = cultureMediaService;
 	}
 
-
-	public List<Video> find_allVideos() throws VideoNotFoundException {
-		List<Video> videos = null;
-		videos = cultureMediaService.findAllVideos();
+	public List<Video> findAllVideos() throws VideoNotFoundException {
+		List<Video> videos = cultureMediaService.findAllVideos();
 		return videos;
 	}
-
 
 }
